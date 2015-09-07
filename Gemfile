652f884b973e25d310ba3e2071f680c06bfb5058
Tony Bowden <tony@tmtm.com>
2015-09-07 14:18:22 +0100

    Initial scraper

---
+++ Gemfile
@@ -0,0 +1,16 @@
+# It's easy to add more libraries or choose different versions. Any libraries
+# specified here will be installed and made available to your morph.io scraper.
+# Find out more: https://morph.io/documentation/ruby
+
+source "https://rubygems.org"
+
+ruby "2.0.0"
+
+gem "scraperwiki", git: "https://github.com/openaustralia/scraperwiki-ruby.git", branch: "morph_defaults"
+gem "execjs"
+gem "pry"
+gem "colorize"
+gem "nokogiri"
+gem "open-uri-cached"
+gem "fuzzy_match"
+gem 'wikidata-client', '~> 0.0.7', require: 'wikidata'
---
+++ scraper.rb
@@ -0,0 +1,61 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+require 'scraperwiki'
+require 'nokogiri'
+require 'colorize'
+require 'pry'
+require 'open-uri/cached'
+OpenURI::Cache.cache_path = '.cache'
+
+class String
+  def tidy
+    self.gsub(/[[:space:]]+/, ' ').strip
+  end
+end
+
+def noko_for(url)
+  Nokogiri::HTML(open(url).read)
+end
+
+def dob_from(node)
+  Date.parse(node.text.tidy[/Born\s+(?:on)\s+(\d+\s+\w+\s+\d+)/, 1]).to_s rescue ''
+end
+
+def scrape_list(url)
+  noko = noko_for(url)
+  noko.css('.liste2 .liste a').each do |a|
+    link = URI.join url, a.attr('href')
+    scrape_mp(a.text, link)
+  end
+end
+
+def scrape_mp(sortname, url)
+  puts url.to_s
+  noko = noko_for(url)
+
+  data = { 
+    id: url.to_s[/id=(\d+)$/, 1],
+    name: noko.css('.pagetitle span').first.text,
+    sortname: sortname, 
+    image: noko.css('.ArticleText2 img/@src').text,
+    party: noko.css('td.Stranka').text.tidy,
+    birth_date: dob_from(noko.css('.ArticleText2')),
+    faction: noko.xpath('//td[b[contains(.,"Deputy club:")]]//a').text,
+    faction_id: noko.xpath('//td[b[contains(.,"Deputy club:")]]//a/@href').text[/id=(\d+)/, 1],
+    constituency: noko.xpath('//td[b[contains(.,"Constituency:")]]/text()').text,
+    start_date: noko.xpath('//td[b[contains(.,"Begin of parliamentary mandate:")]]/text()').text.split('/').reverse.join('-'),
+    # TODO: Chamges, e.g. http://www.sabor.hr/Default.aspx?sec=5358
+    term: 7,
+    source: url.to_s,
+  }
+  data[:image] = URI.join(url, data[:image]).to_s unless data[:image].to_s.empty?
+  if data[:faction].to_s.empty?
+    data[:faction] = "Independent"
+    warn "No faction: setting to #{data[:faction]}".red
+  end
+  # puts data
+  ScraperWiki.save_sqlite([:id, :term], data)
+end
+
+scrape_list('http://www.sabor.hr/Default.aspx?sec=4608')
