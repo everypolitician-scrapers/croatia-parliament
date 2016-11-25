58323e5744d90e548419cd9ed45a1a080a6cfa0a
Oliver Denman <oliver@therewillbeanimals.com>
2016-11-25 15:27:27 +0000

    Extract logic from scrape_mp() to MemberPage class
    As a step towards fully implementing the scraper using Scraped
    gem.

---
+++ lib/member_page.rb
@@ -0,0 +1,50 @@
+require 'scraped'
+
+class MemberPage < Scraped::HTML
+  field :name do
+    noko.xpath('//title').text.split(' - ').last
+  end
+
+  field :image do
+    noko.css('.ArticleText2 img/@src').text
+  end
+
+  field :birth_date do
+    Date.parse(noko.css('.ArticleText2')
+                   .text
+                   .tidy[/Born\s+(?:on)\s+(\d+\s+\w+\s+\d+)/, 1])
+                   .to_s rescue ''
+  end
+
+  field :faction do
+    noko.xpath('//td[b[contains(.,"Deputy club:")]]//a').text
+  end
+
+  field :faction_id do
+    noko.xpath('//td[b[contains(.,"Deputy club:")]]//a/@href').text[/id=(\d+)/, 1]
+  end
+
+  field :party do
+    noko.css('td.Stranka').text.tidy
+  end
+
+  field :constituency do
+    noko.xpath('//td[b[contains(.,"Constituency:")]]/text()').text
+  end
+
+  field :start_date do
+    noko.xpath('//td[b[contains(.,"Begin of parliamentary mandate:")]]/text()')
+        .text
+        .split('/')
+        .reverse
+        .join('-')
+  end
+
+  field :end_date do
+    noko.xpath('//td[b[contains(.,"End of parliamentary mandate:")]]/text()')
+        .text
+        .split('/')
+        .reverse
+        .join('-')
+  end
+end
