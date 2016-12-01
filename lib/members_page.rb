ea92972c10e04a2598559e0a44c4666eefc88386
Oliver Denman <oliver@therewillbeanimals.com>
2016-12-01 11:46:57 +0000

    Extract MembersPage class
    Class represents a single page listing members.

---
+++ lib/members_page.rb
@@ -0,0 +1,9 @@
+require 'scraped'
+
+class MembersPage < Scraped::HTML
+  field :members do
+    noko.css('.liste2 .liste a').map do |a|
+      MemberPage.new(response: Scraped::Request.new(url: a.attr('href')).response)
+    end
+  end
+end
