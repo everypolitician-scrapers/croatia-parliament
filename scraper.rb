cda01da5711327d31180c06ed18e54efb9b34700
Tony Bowden <tony@tmtm.com>
2017-01-03 20:49:50 +0000

    rubocop -a

--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,6 @@
 #!/bin/env ruby
 # encoding: utf-8
+# frozen_string_literal: true
 
 require 'scraperwiki'
 require 'nokogiri'
@@ -11,7 +12,7 @@ require 'scraped_page_archive/open-uri'
 
 class String
   def tidy
-    self.gsub(/[[:space:]]+/, ' ').strip
+    gsub(/[[:space:]]+/, ' ').strip
   end
 end
 
@@ -35,29 +36,29 @@ def scrape_mp(term, sortname, url)
   noko = noko_for(url)
 
   data = {
-    id: url.to_s[/id=(\d+)$/, 1],
-    # name: noko.css('.pagetitle span').first.text,
-    name: noko.xpath('//title').text.split(' - ').last,
-    sortname: sortname,
-    image: noko.css('.ArticleText2 img/@src').text,
-    birth_date: dob_from(noko.css('.ArticleText2')),
-    faction: noko.xpath('//td[b[contains(.,"Deputy club:")]]//a').text,
-    faction_id: noko.xpath('//td[b[contains(.,"Deputy club:")]]//a/@href').text[/id=(\d+)/, 1],
-    party: noko.css('td.Stranka').text.tidy,
+    id:           url.to_s[/id=(\d+)$/, 1],
+    #  name: noko.css('.pagetitle span').first.text,
+    name:         noko.xpath('//title').text.split(' - ').last,
+    sortname:     sortname,
+    image:        noko.css('.ArticleText2 img/@src').text,
+    birth_date:   dob_from(noko.css('.ArticleText2')),
+    faction:      noko.xpath('//td[b[contains(.,"Deputy club:")]]//a').text,
+    faction_id:   noko.xpath('//td[b[contains(.,"Deputy club:")]]//a/@href').text[/id=(\d+)/, 1],
+    party:        noko.css('td.Stranka').text.tidy,
     constituency: noko.xpath('//td[b[contains(.,"Constituency:")]]/text()').text,
-    start_date: noko.xpath('//td[b[contains(.,"Begin of parliamentary mandate:")]]/text()').text.split('/').reverse.join('-'),
-    end_date: noko.xpath('//td[b[contains(.,"End of parliamentary mandate:")]]/text()').text.split('/').reverse.join('-'),
+    start_date:   noko.xpath('//td[b[contains(.,"Begin of parliamentary mandate:")]]/text()').text.split('/').reverse.join('-'),
+    end_date:     noko.xpath('//td[b[contains(.,"End of parliamentary mandate:")]]/text()').text.split('/').reverse.join('-'),
     # TODO: Chamges, e.g. http://www.sabor.hr/Default.aspx?sec=5358
-    term: term,
-    source: url.to_s,
+    term:         term,
+    source:       url.to_s,
   }
   data[:image] = URI.join(url, data[:image]).to_s unless data[:image].to_s.empty?
 
   if data[:faction].to_s.empty?
-    data[:faction] = "Independent"
+    data[:faction] = 'Independent'
     warn "No faction in #{data[:source]}: setting to #{data[:faction]}"
   end
-  ScraperWiki.save_sqlite([:id, :term], data)
+  ScraperWiki.save_sqlite(%i(id term), data)
 end
 
 scrape_list(9, 'http://www.sabor.hr/Default.aspx?sec=4608')
