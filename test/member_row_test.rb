427a6d7d6e12f569d75d25a86784c08fa026a787
Oliver Denman <oliver@therewillbeanimals.com>
2017-02-24 16:06:28 +0000

    Test member row

---
+++ test/member_row_test.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+require_relative './test_helper'
+require_relative '../lib/members_page'
+
+describe MembersPage do
+  around { |test| VCR.use_cassette(File.basename(url), &test) }
+  let(:response)  { MembersPage.new(response: Scraped::Request.new(url: url).response) }
+
+  describe 'Member row' do
+    let(:url) { 'http://www.sabor.hr/Default.aspx?sec=4608' }
+
+    it 'should contain the expected data' do
+      {
+        url:       'http://www.sabor.hr/lgs.axd?t=24&id=7163',
+        sort_name: 'Aleksić, Goran',
+      }.must_equal response.member_rows.first.to_h
+    end
+  end
+end
