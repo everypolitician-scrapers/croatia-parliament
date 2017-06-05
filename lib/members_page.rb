7419465739ed873c96a1c6bee0a4c3107e47573b
Oliver Denman <oliver@mysociety.org>
2017-06-05 11:31:39 +0100

    Rename MemberRow to SeatRow
    In the majority of cases, each row represents the data of a single member. However, in cases where the mandate has ceased, the row also lists the member's substitute. The new name is a better representation of the data on the page.

--- lib/members_page.rb
+++ lib/members_page.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 require 'scraped'
-require_relative 'member_row'
+require_relative 'seat_row'
 
 class MembersPage < Scraped::HTML
   decorator Scraped::Response::Decorator::CleanUrls
 
   field :member_urls do
     noko.css('.liste2 .liste').map do |row|
-      (fragment row => MemberRow).url
+      (fragment row => SeatRow).member_url
     end
   end
 end
--- lib/seat_row.rb
+++ lib/seat_row.rb
@@ -2,16 +2,16 @@
 
 require 'scraped'
 
-class MemberRow < Scraped::HTML
-  field :url do
+class SeatRow < Scraped::HTML
+  field :member_url do
     # A row can contain none, one, or two anchor tags. When one or more links
     # are present, the URL of the member's individual page is always the one
     # containing text that matches their sort name. If no anchors contain
     # the matching text, then the member does not have an individual page.
-    noko.xpath("a[text()='#{sort_name}']/@href").text
+    noko.xpath("a[text()='#{member_sort_name}']/@href").text
   end
 
-  field :sort_name do
+  field :member_sort_name do
     noko.text.split("\r").first.tidy
   end
 end
