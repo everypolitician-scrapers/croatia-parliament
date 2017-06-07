1882a2cb04b470322d5ad88c61817d70c511ed8c
Oliver Denman <oliver@mysociety.org>
2017-06-07 16:09:09 +0100

    Test primary member urls

--- test/seat_row_test.rb
+++ test/seat_row_test.rb
@@ -7,35 +7,35 @@ describe MembersPage do
   around { |test| VCR.use_cassette(File.basename(url), &test) }
   let(:response)  { MembersPage.new(response: Scraped::Request.new(url: url).response) }
 
-  describe 'Member row with single url that points to the member’s page' do
+  describe 'Seat row with single url that points to the primary member’s page' do
     let(:url) { 'http://www.sabor.hr/Default.aspx?sec=4608' }
 
     it 'should contain the expected data' do
-      response.member_urls.first.must_equal 'http://www.sabor.hr/lgs.axd?t=24&id=7163'
+      response.seat_rows.first[:primary_member_url].must_equal 'http://www.sabor.hr/lgs.axd?t=24&id=7163'
     end
   end
 
-  describe 'Member row with single url that does not point to the member’s page' do
+  describe 'Seat row with single url that does not point to the primary member’s page' do
     let(:url) { 'http://www.sabor.hr/dormant-mandates0001' }
 
     it 'returns an empty string for the url' do
-      response.member_urls[1].must_be_empty
+      response.seat_rows[1][:primary_member_url].must_be_empty
     end
   end
 
-  describe 'Member row with two urls' do
+  describe 'Seat row with two urls' do
     let(:url) { 'http://www.sabor.hr/dormant-mandates0001' }
 
-    it 'returns the url that points to the member’s page' do
-      response.member_urls[2].must_equal 'http://www.sabor.hr/lgs.axd?t=24&id=6206'
+    it 'returns the url that points to the primary member’s page' do
+      response.seat_rows[2][:primary_member_url].must_equal 'http://www.sabor.hr/lgs.axd?t=24&id=6206'
     end
   end
 
-  describe 'Member row with no urls' do
+  describe 'Seat row with no urls' do
     let(:url) { 'http://www.sabor.hr/concluded-mandates' }
 
     it 'returns an empty string for the url' do
-      response.member_urls[2].must_be_empty
+      response.seat_rows[2][:primary_member_url].must_be_empty
     end
   end
 end
