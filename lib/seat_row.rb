4ed9ff6d372044eae282f14f9b1cc78b3e8c1734
Oliver Denman <oliver@mysociety.org>
2017-06-07 16:05:06 +0100

    Make SeatRow fields more specific
    A row can have more than one member.

--- lib/seat_row.rb
+++ lib/seat_row.rb
@@ -3,15 +3,15 @@
 require 'scraped'
 
 class SeatRow < Scraped::HTML
-  field :member_url do
+  field :primary_member_url do
     # A row can contain none, one, or two anchor tags. When one or more links
     # are present, the URL of the member's individual page is always the one
     # containing text that matches their sort name. If no anchors contain
     # the matching text, then the member does not have an individual page.
-    noko.xpath("a[text()='#{member_sort_name}']/@href").text
+    noko.xpath("a[text()='#{primary_member_sort_name}']/@href").text
   end
 
-  field :member_sort_name do
+  field :primary_member_sort_name do
     noko.text.split("\r").first.tidy
   end
 end
