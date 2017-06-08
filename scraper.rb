7703f1ebaecb46507587a76b83ec1ff8c698e777
Oliver Denman <oliver@mysociety.org>
2017-06-08 12:22:33 +0100

    Write to db outside of scrape_list method

--- scraper.rb
+++ scraper.rb
@@ -18,24 +18,28 @@ def scrape(h)
   klass.new(response: Scraped::Request.new(url: url).response)
 end
 
-def scrape_list(term, url)
-  scrape(url => MembersPage).member_urls.each do |mem_url|
-    data = scrape(mem_url => MemberPage).to_h.merge(term: term)
-    puts data.reject { |_, v| v.to_s.empty? }.sort_by { |k, _| k }.to_h if ENV['MORPH_DEBUG']
-    ScraperWiki.save_sqlite(%i[id term], data)
+def scrape_list(url)
+  scrape(url => MembersPage).member_urls.map do |mem_url|
+    scrape(mem_url => MemberPage).to_h
   end
 end
 
+member_lists = [
+  { term: 9, url: 'http://www.sabor.hr/Default.aspx?sec=4608' },
+  { term: 9, url: 'http://www.sabor.hr/concluded-mandates' }, # left mid-way
+  { term: 8, url: 'http://www.sabor.hr/members-of-parliament0001' },
+  { term: 8, url: 'http://www.sabor.hr/concluded-mandates0001' }, # left mid-way
+  { term: 8, url: 'http://www.sabor.hr/dormant-mandates0001' }, # suspended memberships
+  { term: 7, url: 'http://www.sabor.hr/members-of-parliament' },
+  { term: 7, url: 'http://www.sabor.hr/0041' }, # left mid-way
+  # { term: 6, url: 'http://www.sabor.hr/Default.aspx?sec=4897' },
+  # { term: 5, url: 'http://www.sabor.hr/Default.aspx?sec=2487' }
+]
+
+data = member_lists.flat_map do |list|
+  scrape_list(list[:url]).map { |mem| mem.merge(term: list[:term]) }
+end
+
+# data.each { |mem| puts mem.reject { |_, v| v.to_s.empty? }.sort_by { |k, _| k }.to_h } if ENV['MORPH_DEBUG']
 ScraperWiki.sqliteexecute('DROP TABLE data') rescue nil
-scrape_list(9, 'http://www.sabor.hr/Default.aspx?sec=4608')
-scrape_list(9, 'http://www.sabor.hr/concluded-mandates') # left mid-way
-
-scrape_list(8, 'http://www.sabor.hr/members-of-parliament0001')
-scrape_list(8, 'http://www.sabor.hr/concluded-mandates0001') # left mid-way
-scrape_list(8, 'http://www.sabor.hr/dormant-mandates0001') # suspended memberships
-
-scrape_list(7, 'http://www.sabor.hr/members-of-parliament')
-scrape_list(7, 'http://www.sabor.hr/0041') # left mid-way
-
-# scrape_list(6, 'http://www.sabor.hr/Default.aspx?sec=4897')
-# scrape_list(5, 'http://www.sabor.hr/Default.aspx?sec=2487')
+ScraperWiki.save_sqlite(%i[id term], data)
