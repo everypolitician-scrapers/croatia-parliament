be6d4ea160c3f762b7d0d7c1e62325e2ad7ce7f0
Oliver Denman <oliver@mysociety.org>
2017-06-08 12:22:33 +0100

    Write to db outside of method

--- scraper.rb
+++ scraper.rb
@@ -19,23 +19,25 @@ def scrape(h)
 end
 
 def scrape_list(term, url)
-  scrape(url => MembersPage).member_urls.each do |mem_url|
+  scrape(url => MembersPage).member_urls.map do |mem_url|
     data = scrape(mem_url => MemberPage).to_h.merge(term: term)
     puts data.reject { |_, v| v.to_s.empty? }.sort_by { |k, _| k }.to_h if ENV['MORPH_DEBUG']
-    ScraperWiki.save_sqlite(%i[id term], data)
+    data
   end
 end
 
+member_lists = [
+  { term: 9, url: 'http://www.sabor.hr/Default.aspx?sec=4608' },
+  { term: 9, url: 'http://www.sabor.hr/concluded-mandates' }, # left mid-way
+  { term: 9, url: 'http://www.sabor.hr/concluded-mandates' },
+  { term: 8, url: 'http://www.sabor.hr/concluded-mandates0001' }, # left mid-way
+  { term: 8, url: 'http://www.sabor.hr/dormant-mandates0001' }, # suspended memberships
+  { term: 7, url: 'http://www.sabor.hr/members-of-parliament' },
+  { term: 7, url: 'http://www.sabor.hr/0041' }, # left mid-way
+  # { term: 6, url: 'http://www.sabor.hr/Default.aspx?sec=4897' },
+  # { term: 5, url: 'http://www.sabor.hr/Default.aspx?sec=2487' }
+]
+
+data = member_lists.flat_map { |list| scrape_list(list[:term], list[:url]) }
 ScraperWiki.sqliteexecute('DROP TABLE data') rescue nil
-scrape_list(9, 'http://www.sabor.hr/Default.aspx?sec=4608')
-scrape_list(9, 'http://www.sabor.hr/concluded-mandates') # left mid-way
-
-scrape_list(8, 'http://www.sabor.hr/members-of-parliament0001')
-scrape_list(8, 'http://www.sabor.hr/concluded-mandates0001') # left mid-way
-scrape_list(8, 'http://www.sabor.hr/dormant-mandates0001') # suspended memberships
-
-scrape_list(7, 'http://www.sabor.hr/members-of-parliament')
-scrape_list(7, 'http://www.sabor.hr/0041') # left mid-way
-
-# scrape_list(6, 'http://www.sabor.hr/Default.aspx?sec=4897')
-# scrape_list(5, 'http://www.sabor.hr/Default.aspx?sec=2487')
+ScraperWiki.save_sqlite(%i[id term], data)
