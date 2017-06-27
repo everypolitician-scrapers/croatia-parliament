509ec1b40bcf3d61300c4cd5e626c285606b5be4
Oliver Denman <oliver@mysociety.org>
2017-06-27 11:16:09 +0100

    Rename scrape_list to member_data
    The new name reflects that this is a query method.

--- scraper.rb
+++ scraper.rb
@@ -18,7 +18,7 @@ def scrape(h)
   klass.new(response: Scraped::Request.new(url: url).response)
 end
 
-def scrape_list(term, url)
+def member_data(term, url)
   scrape(url => MembersPage).member_urls.each do |mem_url|
     data = scrape(mem_url => MemberPage).to_h.merge(term: term)
     puts data.reject { |_, v| v.to_s.empty? }.sort_by { |k, _| k }.to_h if ENV['MORPH_DEBUG']
@@ -27,15 +27,15 @@ def scrape_list(term, url)
 end
 
 ScraperWiki.sqliteexecute('DROP TABLE data') rescue nil
-scrape_list(9, 'http://www.sabor.hr/Default.aspx?sec=4608')
-scrape_list(9, 'http://www.sabor.hr/concluded-mandates') # left mid-way
+member_data(9, 'http://www.sabor.hr/Default.aspx?sec=4608')
+member_data(9, 'http://www.sabor.hr/concluded-mandates') # left mid-way
 
-scrape_list(8, 'http://www.sabor.hr/members-of-parliament0001')
-scrape_list(8, 'http://www.sabor.hr/concluded-mandates0001') # left mid-way
-scrape_list(8, 'http://www.sabor.hr/dormant-mandates0001') # suspended memberships
+member_data(8, 'http://www.sabor.hr/members-of-parliament0001')
+member_data(8, 'http://www.sabor.hr/concluded-mandates0001') # left mid-way
+member_data(8, 'http://www.sabor.hr/dormant-mandates0001') # suspended memberships
 
-scrape_list(7, 'http://www.sabor.hr/members-of-parliament')
-scrape_list(7, 'http://www.sabor.hr/0041') # left mid-way
+member_data(7, 'http://www.sabor.hr/members-of-parliament')
+member_data(7, 'http://www.sabor.hr/0041') # left mid-way
 
-# scrape_list(6, 'http://www.sabor.hr/Default.aspx?sec=4897')
-# scrape_list(5, 'http://www.sabor.hr/Default.aspx?sec=2487')
+# member_data(6, 'http://www.sabor.hr/Default.aspx?sec=4897')
+# member_data(5, 'http://www.sabor.hr/Default.aspx?sec=2487')
